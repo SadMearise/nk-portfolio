1fdbdeb631a83dfd8c0c0c2313897fe22e618127
Nikita Khrustalev <nikita.khrustalev2012@gmail.com>
2025-09-06 15:42:39 +0300

    refactor: fix nav item

--- src/layouts/Header/constants.ts
+++ src/layouts/Header/constants.ts
@@ -2,5 +2,5 @@ export const NAV_ITEMS = [
   { href: "#home", title: "Home" },
   { href: "#about", title: "About" },
   { href: "#work", title: "Work" },
-  { href: "#contacts", title: "Contacts" },
+  { href: "#contact", title: "Contact" },
 ];
