a3d787ffb5865c4d6f4b1e959398be880be95b51
Nikita Khrustalev <nikita.khrustalev2012@gmail.com>
2025-09-13 17:53:40 +0300

    chore: optimize images

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
-    plugins: [react()],
+    plugins: [
+      react(),
+      ViteImageOptimizer({
+        png: {
+          quality: 80,
+        },
+        webp: {
+          lossless: true,
+        },
+      }),
+    ],
     base: command === "build" ? "/nk-portfolio/" : "/",
     build: {
       sourcemap: true,
